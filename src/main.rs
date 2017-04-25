b58a295bdb5b1dc330ba50d3a19513e5fcb421a8
ishiy <y.ishihara.1993@gmail.com>
2017-04-25 19:11:23 +0900

    Improve to use the public calendar

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ extern crate serde_json;
 use std::env;
 use std::fmt;
 use std::io::Read;
+use std::process;
 use chrono::prelude::{DateTime, Local};
 use hyper::client::Client;
 use hyper::header::{Authorization, Basic};
@@ -45,42 +46,58 @@ fn parse_event(v: &serde_json::Value) -> Event {
     }
 }
 
-fn get_ev(name: &str) -> String {
-    let err = "Error: Unable to get ".to_string() + name;
-    env::var(name).expect(&err)
+fn get_ev(name: &str) -> Option<String> {
+    env::var(name).ok()
 }
 
-fn get_sched(url: String, user: String, pass: String) -> Vec<Event> {
+fn get_sched(url: String, user: Option<String>, pass: Option<String>) -> Result<Vec<Event>,String> {
     let ssl = hyper_rustls::TlsClient::new();
     let conn = HttpsConnector::new(ssl);
     let client = Client::with_connector(conn);
+    let req0 = client.get(&url);
 
-    let auth = Authorization(Basic {
-        username: user,
-        password: Some(pass),
-    });
-    let mut res = client.get(&url).header(auth).send()
-                        .expect("Error: Unable to get schedule");
-    let mut res_body = String::new();
-    res.read_to_string(&mut res_body).unwrap();
-    let vs : serde_json::Value = serde_json::from_str(&res_body).unwrap();
-    vs.as_array().unwrap().iter().map(parse_event).collect()
+    let req = match user {
+        Some(u) =>
+            req0.header(Authorization(Basic {
+                username: u,
+                password: pass,
+            })),
+        _ =>  req0,
+    };
+
+    let mut res = try!(req.send().map_err(|e| e.to_string()));
+
+    if res.status.is_success() {
+        let mut res_body = String::new();
+        res.read_to_string(&mut res_body).unwrap();
+        let vs : serde_json::Value = serde_json::from_str(&res_body).unwrap();
+        Ok(vs.as_array().unwrap().iter().map(parse_event).collect())
+    } else {
+        Err(res.status.to_string())
+    }
 }
 
 fn main() {
-    let url = get_ev("CAMPH_SCHED_URL");
+    let url = get_ev("CAMPH_SCHED_URL").unwrap();
     let user = get_ev("CAMPH_SCHED_USER");
     let pass =  get_ev("CAMPH_SCHED_PASS");
-    let es = get_sched(url, user, pass);
 
-    let today = Local::today();
-    let next = today + chrono::Duration::days(7);
+    match get_sched(url, user, pass) {
+        Err(e) => {
+            println!("Error: {}", e);
+            process::exit(1);
+        },
+        Ok(es) => {
+            let today = Local::today();
+            let next = today + chrono::Duration::days(7);
 
-    let mut res : Vec<&Event> = es.iter()
-        .filter(|e| e.start.date() >= today && e.end.date() < next)
-        .collect();
-    &res.sort_by_key(|e| e.start);
-    for e in res {
-        println!("{}", e);
+            let mut res : Vec<&Event> = es.iter()
+                .filter(|e| e.start.date() >= today && e.end.date() < next)
+                .collect();
+            &res.sort_by_key(|e| e.start);
+            for e in res {
+                println!("{}", e);
+            }
+        },
     }
 }
